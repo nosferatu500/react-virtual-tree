87ef0430ed5c6a47bf2ef78c4e4b4fa1fd4963ef
Vitalii <vitalya199529@gmail.com>
2024-08-30 10:19:42 +0900

    Use root folder

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,31 @@ import VTree from './VTree';
 
 const initialTreeData: TNode[] = [
     {
-        id: "1",
-        name: 'src',
+        id: "root",
+        name: 'root',
         type: 'folder',
         children: [
-            { id: "1.1", name: 'index.js', type: 'file', children: [] },
-            { id: "1.2", name: 'App.js', type: 'file', children: [] },
             {
-                id: "1.3",
-                name: 'components',
+                id: "1",
+                name: 'src',
                 type: 'folder',
                 children: [
-                    { id: "1.3.1", name: 'Header.js', type: 'file', children: [] },
-                    { id: "1.3.2", name: 'Footer.js', type: 'file', children: [] },
+                    { id: "1.1", name: 'index.js', type: 'file', children: [] },
+                    { id: "1.2", name: 'App.js', type: 'file', children: [] },
+                    {
+                        id: "1.3",
+                        name: 'components',
+                        type: 'folder',
+                        children: [
+                            { id: "1.3.1", name: 'Header.js', type: 'file', children: [] },
+                            { id: "1.3.2", name: 'Footer.js', type: 'file', children: [] },
+                        ],
+                    },
                 ],
             },
-        ],
+            { id: "2", name: 'package.json', type: 'file', children: [] },
+        ]
     },
-    { id: "2", name: 'package.json', type: 'file', children: [] },
 ];
 
 
